47972dc8978cd2ef64cbc42fa18b86c9e82e7a7f
rgillis8 <rgillis@mit.edu>
2017-07-14 14:28:38 -0400

    clean up parsing

--- rmgpy/cantherm/gaussian.py
+++ rmgpy/cantherm/gaussian.py
@@ -86,6 +86,7 @@ class GaussianLog:
 
         Natoms = self.getNumberOfAtoms()
         Nrows = Natoms * 3
+        Nentries = (Natoms*(Natoms+1))/2
 
         f = open(self.path, 'r')
         line = f.readline()
@@ -114,6 +115,8 @@ class GaussianLog:
             datalist[i] = datalist[i].replace("@","")
             datalist[i] = datalist[i].replace(" ","")
             n = len(datalist)
+
+        #print(datalist)
         m = 0
         for k in range(0,Nrows):
             for l in range(0,Nrows):
@@ -124,7 +127,9 @@ class GaussianLog:
                     m += 1
         # Convert from atomic units (Hartree/Bohr_radius^2) to J/m^2
         F *= 4.35974417e-18 / 5.291772108e-11**2
-
+        #print(Nrows)
+        #print(F)
+        #raise Exception('gets here')
         ####################Old Method#################################################
         #while line != '':
         #    # Read force constant matrix
@@ -143,7 +148,6 @@ class GaussianLog:
         #        F *= 4.35974417e-18 / 5.291772108e-11**2
         #    line = f.readline()
         # Close file when finished
-
         f.close()
 
         return F
